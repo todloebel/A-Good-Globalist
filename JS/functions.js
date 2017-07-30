131ca464c0160e5c043f81d90efe96c413263a8d
Tod Loebel <todloebel@gmail.com>
2017-07-29 20:55:40 -0400

    edits

--- JS/functions.js
+++ JS/functions.js
@@ -1,21 +1,44 @@
+function showAbout(){
+    $("#showAbout").click(function(){
+        $("#ABOUT").slideToggle("slow");
+    });
+};
 
+$(document).ready(function showMovement(){
+    $("#showMovement").click(function(){
+        $("#MOVE").slideToggle("slow");
+    });
+});
 
+$(document).ready(function showTrade(){
+    $("#showTrade").click(function(){
+        $("#TRADE").slideToggle("slow");
+    });
+});
 
+$(document).ready(function showAccess(){
+    $("#showAccess").click(function(){
+        $("#ACCESS").slideToggle("slow");
+    });
+});
 
+(function () {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function startTime() {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds());
+        document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+})();
 
 (function( $ ){
 
